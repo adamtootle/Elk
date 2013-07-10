a99442ef61c3025888d0c621579c0fb8c2673d7b
Adam Tootle <adamtootle@gmail.com>
2013-07-10 19:36:10 -0400

    Initial commit. Fresh rails install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elk::Application.config.secret_token = '00fa56abd7d52d72c87908753f3cfb88db62d0b40700eb29e3c1a9fb8b126c3507d6d6888e6995eb7518843be77f91f3084258a47566b0c352c5f93b4fdeb711'
